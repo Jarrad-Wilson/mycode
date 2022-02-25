8c5eb9ec9c068629f78d5f2b7b912088e42ce70b
Jarrad Wilson <Jarrad-Wilson@users.noreply.github.com>
2022-02-25 23:05:49 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
 
-# display only the IP addresses to the screen.
+print("The first item in the list is:" + " " + my_list[0])
+
+print('The second item in the list is:' + " " + str(my_list[1]))
+
+print("The last item in the list is:" + " " + my_list[2])
+
 iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-# example 1 - add up the strings
-print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+print("The IP Addresses are:" + iplist[3] + " and "  + iplist[4])
+
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
+print("IP addresses:", iplist[3], ", and", iplist[4])
