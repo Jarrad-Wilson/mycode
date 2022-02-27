b92c4c4cb5ba24ccb6d48901e491d966608e48d5
Jarrad Wilson <Jarrad-Wilson@users.noreply.github.com>
2022-02-27 05:07:45 +0000

    learning about dictionaries

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -1,46 +1,37 @@
 #!/usr/bin/env python3
 
-## create a dictionary
-switch = {"hostname": "sw1", "ip": "10.0.1.1", "version": "1.2", "vendor": "cisco"}
+# Create a dictionary
+switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
 
-## display parts of the dictionary
-print( switch["hostname"] )
-print( switch["ip"] )
+#Display parts of the disctionary
+print(switch['hostname'])
+print(switch['ip'])
 
-## request a 'fake' key
-# print( switch["lynx"] )   # Be sure to comment out this line,
-                            # or your program will CONTINUE to fail!
-                            # if a KEY is requested that does not exist,
-                            # an ERROR will be thrown!
+# Request a 'fake' key
+# print( switch['lynx'] ) # Be sure to comment out th                          #is line, or your program w                          #ill continue to fail! If a                          #key is requested that does                          #not exist, an error will b                          #e thrown!
+# Request a 'fake' key with .get() method
+print('\nfirst test - .get():')
+print(switch.get('lynx', 'THE KEY IS IN ANOTHER CASTLE!'))
+print('\nThird test - .get():')
+print(switch.get('version'))
 
-## request a 'fake' key with .get() method
-print( "First test - .get()" )
-print( switch.get("lynx") )
+print('\nFourth test - .keys():')
+print(switch.keys())
 
-print( "Second test - .get()" )
-print( switch.get("lynx", "THE KEY IS IN ANOTHER CASTLE!") )
+print('\nFifth test - .values():')
+print(switch.values())
 
-print( "Third test - .get()" )
-print( switch.get("version") )
+print( "\nSixth test - .pop():")
+switch.pop('version') # Removes this key and value pair
+print(switch.keys()) # Notice the value of version is gone
 
-print( "Fourth test - .keys()" )
-print( switch.keys() )
+print('\nSeventh test - ADD a new value: \n')
+switch['adminlogin'] = 'karl08'
+print(switch.keys())
+print(switch.values())
 
-print( "Fifth test - .values()" )
-print( switch.values() )
 
-print( "Sixth test - .pop()" )
-switch.pop("version") # removes this key (and value) pair
-print( switch.keys() )   # notice the value of version is gone
-print( switch.values() ) # notice the value 1.2
-
-print( "Seventh test - ADD a new value" )
-switch["adminlogin"] = "karl08"
-print( switch.keys() )
-print( switch.values() )
-
-print( "Eighth test - ADD a new value" )
-switch["password"] = "qwerty"
-print( switch.keys() )
-print( switch.values() )
-
+print('\nEigth test - ADD a new value: \n')
+switch['password'] = 'qwerty'
+print(switch.keys())
+print(switch.values())
