b959e72b18487eebe593a8ba8cc3af75f3e13c4e
Jarrad Wilson <Jarrad-Wilson@users.noreply.github.com>
2022-02-27 02:51:03 +0000

    About complex lists

--- advlist/complex01.py
+++ advlist/complex01.py
@@ -1,41 +1,30 @@
 #!/usr/bin/env python3
-"""Alta3 Research | RZFeeser
-   List - making selections from complex lists"""
 
-def main():
+# Create a list called list1
+list1 = ['cisco_nxos', 'artista_eos', 'cisco_ios']
 
-    # create a list called list1
-    list1 = ["cisco_nxos", "arista_eos", "cisco_ios"]
-    
-    # display list1
-    print(list1)
-    
-    # display list1[1] which should only display arista_eos
-    print(list1[1])
-    
-    # create a new list containing a single item
-    list2 = ["juniper"]
-    
-    # extend list1 by list2 (combine both lists together into a single list)
-    list1.extend(list2)
-    
-    # display list1, which now contains juniper
-    print(list1)
-    
-    # create list3
-    list3 = ["10.1.0.1", "10.2.0.1", "10.3.0.1"]
-    
-    # use the append operation to append list1 by list3
-    list1.append(list3)
-    
-    # display the new complex list1
-    print(list1)
-    
-    # display the list of IP addresses
-    print(list1[4])
-    
-    # display the first item in the list (0th item) - first IP address
-    print(list1[4][0])
+print(list1) # Display list1
 
-main()
+# Print the second item in the list
+print(list1[1])
 
+# Add second list, then combine list1 and lis2
+list2 = ['juniper']
+list1.extend(list2)
+
+# Print results 
+print(list1)
+
+# Create list3 and append it to list1
+
+list3 = ['10.1.0.1', '10.2.0.1', '10.3.0.1']
+list1.append(list3)
+
+# Print the results
+print(list1)
+
+# Print element 5 in list1
+print(list1[4])
+
+animals = ['dog', 'cat', 'rat', 'cow']
+print(animals)
